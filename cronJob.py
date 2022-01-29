a68df5bbbdda3fa282669765b7c8fcd40154cdb0
Zach Avila <zachavila1@gmail.com>
2022-01-29 14:46:54 -0500

    #INTIAL PROJECT
    --cronJob.py
    Still in Beta Testing
    
    --debug_access_token.py
    Gets Valid/expiration dates for the access token
    
    --defines.py
    Main class used to access the API, using verification credentials
    
    --get_instagram_account.py
    This will get you your Instagram account ID
    
    --get_long_live_access_token
    Gets access token from temp and will remain valid for 3 months
    
    --get_user_pages.py
    This class will get the facebook page ID
    
    --main.py
    Main class, after credentials are entered this access the mongo db, get image urls and upload them to instagram
    
    ####ADDED POSTY_CONTENT.PY
    Missed in the last commit, this will be used to do the work and post the video.

--- cronJob.py
+++ cronJob.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 
 if __name__ == '__main__':
     cron = CronTab()
-    job = cron.new(command='main.py')
+    job = cron.new(command='echo hello_world')
 
     job.minute.every(1)
-    cron.run_pending()
+    cron.write()
